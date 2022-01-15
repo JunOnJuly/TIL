d083c4994088af9b3cfbf83707ea7435bf27d903
JunOnJuly <wlgnstjd0717@gmail.com>
2022-01-16 00:39:23 +0900

    update 1003.py

--- python_code/BaekJoon/1003.py
+++ python_code/BaekJoon/1003.py
@@ -1,10 +1,12 @@
 # 피보나치 함수 정의
 def fibonacci(num: int):
     if num == 0:
-        call_0.append(0)
+        global call_0
+        call_0 += 1
         return 0
     elif num == 1:
-        call_1.append(1)
+        global call_1
+        call_1 += 1
         return 1
     else:
         return fibonacci(num - 1) + fibonacci(num - 2)
@@ -12,13 +14,11 @@ def fibonacci(num: int):
 
 # 데이터 입력
 test_time = int(input())
-num_list = []
-for i in range(test_time):
-    num_list.append(int(input()))
 
-# 출력
-for i in num_list:
-    call_0 = []
-    call_1 = []
-    fibonacci(i)
-    print("{0} {1}".format(len(call_0), len(call_1)))
+for i in range(test_time):
+    global call_0
+    call_0 = 0
+    global call_1
+    call_1 = 0
+    fibonacci(int(input()))
+    print("{0} {1}".format(call_0, call_1))
