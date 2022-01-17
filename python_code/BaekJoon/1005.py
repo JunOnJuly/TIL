f3e14e7730432e1f967f7cdbafc723fc6764b133
JunOnJuly <wlgnstjd0717@gmail.com>
2022-01-17 22:50:05 +0900

    update 1005.py

--- python_code/BaekJoon/1005.py
+++ python_code/BaekJoon/1005.py
@@ -1,34 +1,55 @@
-def find_ness_build(building_num: int) -> list:
-    ness_building = [building_num]
-
-    for rule in range(build_rule):
-        if building_num == rule[1]:
+# 함수 정의--------------------------------------------------------------------------------------------------------------
+def find_ness_build(win_rule: int, build_rule: list) -> list:
+    for rule in build_rule:
+        if win_rule == rule[1]:
+            find_ness_build(rule[0], build_rule)
             if rule[0] not in ness_building:
                 ness_building.append(rule[0])
             else:
                 continue
-        
+    return ness_building
+
+
+def clean_up(build_rule: list) -> list:
+    cleaned_list = []
+    for i in range(max(build_rule)[1]):
+        cleaned_list.append([])
+    for i in range(len(build_rule)):
+        cleaned_list[build_rule[i][1] - 1].append(build_rule[i][0])
+        cleaned_list[build_rule[i][1] - 1].sort()
+    return cleaned_list
+
 
 # 데이터 입력-------------------------------------------------------------------------------------------------------------
+global ness_building
+ness_building = []
 
 # 테스트 횟수
 num_case = int(input())
 
-# 건물의 수, 규칙의 수
-num_build, num_rule = map(int, input().split())
+for i in range(num_case):
+    # 건물의 수, 규칙의 수
+    num_build, num_rule = map(int, input().split())
 
-# 건물 당 걸리는 시간
-time_build = []
-for i in range(num_build):
-    time_build.append(int(input()))
+    # 건물 당 걸리는 시간
+    time_build = []
+    time_build.append(list(map(int, input().split())))
 
-# 건설순서
-build_rule = []
-for i in range(num_rule):
-    build_rule.append(list(map(int, input().split())))
+    # 건설순서
+    build_rule = []
+    for j in range(num_rule):
+        build_rule.append(list(map(int, input().split())))
 
-# 승리조건
-win_rule = int(input())
+    # 승리조건
+    win_rule = int(input())
 
-# 데이터 검색-------------------------------------------------------------------------------------------------------------
+    # 계산-----------------------------------------------------------------------------------------------------
 
+    # 필요한 건물 계산
+    ness_building = sorted(find_ness_build(win_rule, build_rule))
+    cleaned_list = clean_up(build_rule)
+
+    print(ness_building)
+    print(cleaned_list)
+
+    # 필요한 건물 & 건설 순서 정리
