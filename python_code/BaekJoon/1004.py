25566ad49d522a11c1a7ff20ee02c181db16fd1e
JunOnJuly <wlgnstjd0717@gmail.com>
2022-01-17 14:54:26 +0900

    update 1004.py

--- python_code/BaekJoon/1004.py
+++ python_code/BaekJoon/1004.py
@@ -1,10 +1,14 @@
 # 테스트 횟수
 test_times = int(input())
 
+# 데이터 구조는 [[[출발점과 도착점의 좌표1][출발점과 도착점의 좌표2]..], [행성계의 개수1, 행성계의 개수2], [[행성계의 좌표1][행성계의 좌표2]..]]
 # 데이터 입력 및 저장
+data = [[], [], []]
 for i in range(test_times):
-    data_temp = [list(map(int, input().split()))]
+    data_temp = list(map(int, input().split()))
+    data[0].append(data_temp)
     num_star = int(input())
-    data_temp.append([num_star])
+    data[1].append(num_star)
     for j in range(num_star):
-        data_temp.append(list(map(int, input().split())))
+        data[2].append(list(map(int, input().split())))
+print(data)
