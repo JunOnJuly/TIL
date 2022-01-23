05f6303110481abde7ce8583e16ab4eb34ea2d2c
JunOnJuly <wlgnstjd0717@gmail.com>
2022-01-23 15:50:59 +0900

    update 1003

--- python_code/BaekJoon/1003.py
+++ python_code/BaekJoon/1003.py
@@ -2,17 +2,16 @@
 fib_num = []
 fib_num.append(0)
 fib_num.append(1)
-for i in range(40):
+for i in range(39):
     fib_num.append(fib_num[i] + fib_num[i + 1])
 
 
-
 # 데이터 입력
-test_time = int(input())
+test_time = int(input().strip())
 
 for i in range(test_time):
-    input_num = int(input())
+    input_num = int(input().strip())
     if input_num == 0:
         print(f'{1} {0}')
     else:
-        print(f'{fibonacci(input_num - 1)} {fibonacci(input_num)}')
+        print(f'{fib_num[input_num - 1]} {fib_num[input_num]}')
