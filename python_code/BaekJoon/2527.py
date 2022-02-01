691f2c2eb7a81573ad310a31c85b1480b06a2242
JunOnJuly <wlgnstjd0717@gmail.com>
2022-02-02 00:19:49 +0900

    update 1005.py

--- python_code/BaekJoon/2527.py
+++ python_code/BaekJoon/2527.py
@@ -5,33 +5,35 @@ for _ in range(4):
 
     point_2_temp = point_2[2:] + point_2[:2]
 
-    print(point_1)
-    print(point_2)
-    print(point_2_temp)
-    for i in range(len(point_1)):
-        switch = 0
-        bool_1 = (point_1[i][0] > point_2_temp[i][0])
-        bool_2 = (point_1[i][0] < point_2_temp[i][0])
-        bool_3 = (point_1[i][1] > point_2_temp[i][1])
-        bool_4 = (point_1[i][1] < point_2_temp[i][1])
+    switch = 0
 
-        for j in range(len(point_1)):
-            if point_1[j] == point_2_temp[j]:
-                print('c')
-                switch = 1
-                break
-        if switch == 1:
+    for j in range(4):
+        if point_1[j] == point_2_temp[j]:
+            print('c')
+            switch = 1
             break
+    if switch == 1:
+        continue
 
-        if ((i == 0 or i == 3) and bool_1) or ((i == 0 or i == 1) and bool_3) \
-                or ((i == 1 or i == 2) and bool_2) or ((i == 2 or i == 3) and bool_4):
+    for j in range(0, 4, 2):
+        if (j == 0) and ((point_1[j][0] > point_2_temp[j][0]) or (point_1[j][1] > point_2_temp[j][1])):
             print('d')
+            switch = 1
             break
-        elif (point_1[i][0] == point_2_temp[i][0]) or (point_1[i][1] == point_2_temp[i][1]):
-            print('b')
+        elif (j == 2) and ((point_1[j][0] < point_2_temp[j][0]) or (point_1[j][1] < point_2_temp[j][1])):
+            print('d')
+            switch = 1
             break
         else:
-            if i == len(point_1) - 1:
-                print('a')
-                break
             continue
+    if switch == 1:
+        continue
+
+    for j in range(4):
+        if (point_1[j][0] == point_2_temp[j][0]) or (point_1[j][1] == point_2_temp[j][1]):
+            print('b')
+            switch = 1
+            break
+    if switch == 1:
+        continue
+    print('a')
