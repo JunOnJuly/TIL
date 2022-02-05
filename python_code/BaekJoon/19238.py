042e8296c8c1c6e0db40139ca591a1244e28c0ad
JunOnJuly <wlgnstjd0717@gmail.com>
2022-02-05 21:28:57 +0900

    update 0205

---
+++ python_code/BaekJoon/19238.py
@@ -0,0 +1,3 @@
+m, n = map(int, input().split())
+
+num_bus = int(input())
--- python_code/BaekJoon/sdfsadfsad.py
+++
@@ -1,9 +0,0 @@
-for i in range(5):
-    switch = 0
-    for j in range(5):
-        if ~
-            switch = 1
-            break
-    if switch == 1:
-        break
-
