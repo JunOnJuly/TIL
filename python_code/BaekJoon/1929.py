0b065fa9b186491cfdd518c261cfe7260bf4d673
JunOnJuly <wlgnstjd0717@gmail.com>
2022-02-20 15:07:51 +0900

    update 0220.py

--- python_code/BaekJoon/1929.py
+++ python_code/BaekJoon/1929.py
@@ -1,24 +1,20 @@
+from math import sqrt
+
 M, N = map(int, input().split())
 
-list_print = [2] + list(range(3, N + 1, 2))
+list_print = [2]
 
-i = 0
-while True:
-    if i >= N // 2 + 1:
-        break
-    if i >= len(list_print) - 1:
-        break
-    num = list_print[i]
-    j = i + 1
-    while True:
-        if j == len(list_print):
+for num in range(3, N + 1, 2):
+    for i in range(len(list_print)):
+        if not num % list_print[i]:
             break
-        if not list_print[j] % list_print[i]:
-            del list_print[j]
-        else:
-            j += 1
-    i += 1
+        if i >= sqrt(num) + 1:
+            list_print.append(num)
+            break
+        if i == len(list_print) - 1:
+            list_print.append(num)
+            break
 
 for num in list_print:
-    if M <= num <= N:
+    if num >= M:
         print(num)
