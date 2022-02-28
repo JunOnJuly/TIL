721dbe4eb941e56c63642719e6288bca37e41a60
JunOnJuly <wlgnstjd0717@gmail.com>
2022-02-28 19:11:53 +0900

    update 0228.py

--- python_code/BaekJoon/2231.py
+++ python_code/BaekJoon/2231.py
@@ -1,42 +1,27 @@
 N = int(input())
 
 switch = 0
+str_N = str(N)
 
-if len(str(N)) == 1:
-    print(0)
-
+if N <= len(str_N)*9:
+    range_num = 0
 else:
-    if N > len(str(N))*9:
-        for num in range(N-len(str(N))*9, N + 1):
-            if switch == 1:
-                break
-            str_num = str(num)
-            num_sum = num + 0
+    range_num = N - len(str_N)*9
 
-            for i in range(len(str_num)):
-                num_sum += int(str_num[i])
+for num in range(range_num, N + 1):
+    if switch == 1:
+        break
+    sum_num = num + 0
+    str_num = str(num)
 
-            if num_sum == N:
-                print(num)
-                switch = 1
-                break
+    for i in range(len(str_num)):
+        if sum_num > N:
+            break
+        sum_num += int(str_num[i])
 
-            elif num == N:
-                print(0)
-    else:
-        for num in range(0, N + 1):
-            if switch == 1:
-                break
-            str_num = str(num)
-            num_sum = num + 0
-
-            for i in range(len(str_num)):
-                num_sum += int(str_num[i])
-
-            if num_sum == N:
-                print(num)
-                switch = 1
-                break
-
-            elif num == N:
-                print(0)
+    if sum_num == N:
+        print(num)
+        switch = 1
+        break
+    elif num == N:
+        print(0)
