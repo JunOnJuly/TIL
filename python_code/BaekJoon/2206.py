e1ea4c1a4f86e2479b1eb938cb98b49fb34fc866
JunOnJuly <wlgnstjd0717@gmail.com>
2022-03-24 17:15:47 +0900

    update 0324.py

--- python_code/BaekJoon/2206.py
+++ python_code/BaekJoon/2206.py
@@ -1,59 +1,44 @@
 from collections import deque
 
 
-def make_route(map_data, break_idx):
-    map_data[breakable_wall[break_idx][0]][breakable_wall[break_idx][1]] = 0
-    print(map_data)
-    que = deque([[0, 0]])
-
-    cnt = 1
+def make_route(map_data, num_break, que_list, cnt):
+    global min_cnt
+    if que_list[-1] == [N - 1, M - 1]:
+        if cnt< min_cnt:
+            min_cnt = cnt
+        return
     while True:
-        if not que:
-            return -1
-        for i in range(len(que)):
-            pop_que = que.popleft()
-            i_now = pop_que[0]
-            j_now = pop_que[1]
+        if not que_list:
+            return
+        for i in range(len(que_list)):
+            pop_que = que_list.popleft()
             for j in range(4):
-                i_next = i_now + guide_move_i[j]
-                j_next = j_now + guide_move_j[j]
-                if 0 <= i_next < N and 0 <= j_next < M and map_data[i_next][j_next] != 1:
-                    que.append([i_next, j_next])
-                    map_data[i_next][j_next] = 1
-                    if que[-1] == [N - 1, M - 1]:
-                        cnt += 1
-                        return cnt
-        cnt += 1
+                i_next = pop_que[0] + guide_move_i[j]
+                j_next = pop_que[1] + guide_move_j[j]
+                if 0 <= i_next < N and 0 <= j_next < M:
+                    if num_break == 0 and map_data[i_next][j_next] == 1:
+                        num_break = 1
+                        que_list.append([i_next, j_next])
+                        make_route(map_data, num_break, que_list + deque([]), cnt + 1)
+                        num_break = 0
+                        que_list.pop()
+                    elif map_data[i_next][j_next] != 1:
+                        map_data[i_next][j_next] = 1
+                        que_list.append([i_next, j_next])
+                        make_route(map_data, num_break, que_list + deque([]), cnt + 1)
+                        que_list.pop()
+                        map_data[i_next][j_next] = 0
 
+
+N, M = map(int, input().split())
 
 guide_move_i = [-1, 0, 1, 0]
 guide_move_j = [0, 1, 0, -1]
 
-N, M = map(int, input().split())
-
 data_input = [list(map(int, list(input()))) for _ in range(N)]
-
-breakable_wall = []
-for i in range(len(data_input)):
-    for j in range(len(data_input[i])):
-        if data_input[i][j] == 1:
-            cnt_road = 0
-            for k in range(4):
-                i_now = i + guide_move_i[k]
-                j_now = j + guide_move_j[k]
-                if 0 <= i_now < N and 0 <= j_now < M:
-                    if data_input[i_now][j_now] == 0:
-                        cnt_road += 1
-                if cnt_road >= 2:
-                    breakable_wall.append([i, j])
-                    break
-
 min_cnt = M * N
-for i in range(len(breakable_wall)):
-    cnt_route = make_route(data_input, i)
-    if cnt_route != -1 and cnt_route < min_cnt:
-        min_cnt = cnt_route
 
+make_route(data_input, 0, deque([[0, 0]]), 1)
 if min_cnt != M * N:
     print(min_cnt)
 else:
