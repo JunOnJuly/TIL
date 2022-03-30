56db4087caf319ed2e18dca196d4d8e0ad66e0e1
JunOnJuly <wlgnstjd0717@gmail.com>
2022-03-30 09:50:52 +0900

    update 0330.py

--- python_code/BaekJoon/2206.py
+++ python_code/BaekJoon/2206.py
@@ -2,32 +2,39 @@ from collections import deque
 
 
 def make_route(map_input):
-    que = deque([[0, 0, 1]])
-    visited = [[[-1, -1] for __ in range(M)] for _ in range(N)]
-    visited[0][0][0] = 1
+    que = deque([[0, 0, 0]])
+    visited = [[[1, 1] for _ in range(M)] for __ in range(N)]
+    cnt = 1
     while True:
         if not que:
             return -1
-        i_now, j_now, break_check = que.popleft()
-        if i_now == N - 1 and j_now == M - 1:
-            return max(visited[-1][-1])
-        for j in range(4):
-            i_next = i_now + guide_move_i[j]
-            j_next = j_now + guide_move_j[j]
-            if i_next < 0 or i_next >= N or j_next < 0 or j_next >= M:
-                continue
+        for i in range(len(que)):
+            i_now, j_now, breakable = que.popleft()
+            if [i_now, j_now] == [N - 1, M - 1]:
+                return cnt
+            for j in range(4):
+                i_next = i_now + guide_move_i[j]
+                j_next = j_now + guide_move_j[j]
 
-            if map_input[i_next][j_next] and break_check:
-                visited[i_next][j_next][1] = visited[i_now][j_now][0] + 1
-                que.append([i_next, j_next, 0])
+                if i_next < 0 or i_next >= N or j_next < 0 or j_next >= M:
+                    continue
 
-            if not map_input[i_next][j_next]:
-                if visited[i_next][j_next][0] < 0:
-                    visited[i_next][j_next][0] = visited[i_now][j_now][0] + 1
-                    que.append([i_next, j_next, break_check])
-                if visited[i_next][j_next][1] < 0:
-                    visited[i_next][j_next][1] = visited[i_now][j_now][1] + 1
-                    que.append([i_next, j_next, break_check])
+                if map_input[i_next][j_next] and not breakable and visited[i_next][j_next]:
+                    visited[i_next][j_next][1] = 0
+                    que.append([i_next, j_next, 1])
+
+                if not map_input[i_next][j_next] and visited[i_next][j_next][0] and not breakable:
+                    que.append([i_next, j_next, 0])
+                    visited[i_next][j_next][0] = 0
+                    if que[-1][:2] == [N - 1, M - 1]:
+                        return cnt + 1
+
+                if not map_input[i_next][j_next] and visited[i_next][j_next][1] and breakable:
+                    que.append([i_next, j_next, 1])
+                    visited[i_next][j_next][1] = 0
+                    if que[-1][:2] == [N - 1, M - 1]:
+                        return cnt + 1
+        cnt += 1
 
 
 N, M = map(int, input().split())
@@ -36,6 +43,5 @@ guide_move_i = [0, 1, 0, -1]
 guide_move_j = [1, 0, -1, 0]
 
 data_input = [list(map(int, list(input()))) for _ in range(N)]
-broke = [[1] * M for _ in range(N)]
 
 print(make_route(data_input))
