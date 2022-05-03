a377d0b1eea2d08b60d6921fbd7afee73cbe3e64
JunOnJuly <wlgnstjd0717@gmail.com>
2022-05-03 16:10:43 +0900

    update 0504

--- python_code/BaekJoon/MooTube.py
+++ python_code/BaekJoon/MooTube.py
@@ -1,7 +1,9 @@
 N, Q = map(int, input().split())
 
-usado = [list(map(int, input().split())) for i in range(N - 1)]
+usados = [list(map(int, input().split())) for i in range(N - 1)]
 questions = [list(map(int, input().split())) for i in range(Q)]
 
 map_straight = [[] for i in range(N + 1)]
+
+stack = []
 
