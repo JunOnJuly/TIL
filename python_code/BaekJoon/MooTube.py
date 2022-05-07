db3a6303917d1510da4a93008c10bad648df5b4b
JunOnJuly <wlgnstjd0717@gmail.com>
2022-05-07 16:26:18 +0900

    update 0507

--- python_code/BaekJoon/MooTube.py
+++ python_code/BaekJoon/MooTube.py
@@ -4,16 +4,3 @@ N, Q = map(int, input().split())
 
 usados = [list(map(int, input().split())) for i in range(N - 1)]
 questions = [list(map(int, input().split())) for i in range(Q)]
-
-map_straight = usados[::]
-
-while True:
-    if not usados:
-        break
-
-    que = deque([usados.pop()])
-
-    while True:
-        if not que:
-            break
-
