fc0c6f54ced1bac1c989ce139eaa815bfb17000e
JunOnJuly <wlgnstjd0717@gmail.com>
2022-05-13 17:12:11 +0900

    update

--- SASM/logical.asm
+++ SASM/logical.asm
@@ -18,8 +18,12 @@ CMAIN:
     shr eax, 8
     PRINT_HEX 4, eax
     NEWLINE
+    ; 곱셈/ 나눗셈에 용이
     
-    xor rax, rax
+    ; xor 은 암호학에 용
+    
+    
+    xor rax, rax ; 아무 의미 없다는 의미로 rax에 0을 대입
     ret
 
 section .data
