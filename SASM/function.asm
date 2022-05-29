135c631a90c0ad950ebea67c9b6a5a27302bf3e8
JunOnJuly <wlgnstjd0717@gmail.com>
2022-05-29 18:03:53 +0900

    update

--- SASM/function.asm
+++ SASM/function.asm
@@ -3,6 +3,29 @@
 section .text
 global CMAIN
 CMAIN:
-    ;write your code here
+    ; 함수
+    
+    call PRINT_MSG
+    
     xor rax, rax
     ret
+    
+print_MSG:
+    PRINT_STRING msg
+    NEWLINE
+    ret
+    
+MAX:
+    cmp eax, ebx
+    jg L1
+    mov ecx, ebx
+
+L1:
+    mov ecx, eax
+L2:
+    ret
+    
+    
+section .data
+    msg db 'HELLO World', 0x00
+section .bss
