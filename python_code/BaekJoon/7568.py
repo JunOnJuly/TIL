04b35c888f6bf6c810acc9fbba3a0b0ca59cbb3a
JunOnJuly <wlgnstjd0717@gmail.com>
2022-06-16 18:30:19 +0900

    220616

--- python_code/BaekJoon/7568.py
+++ python_code/BaekJoon/7568.py
@@ -1,10 +1,16 @@
 N = int(input())
 
-data_input_height = []
-data_input_weight = []
-for _ in range(N):
+data_input = []
 
+for _ in range(N):
+    data_input.append(list(map(int, input())) + [1])
 
 print(data_input)
 
 for i in range(N):
+    for j in range(N):
+        if i == j:
+            continue
+        if data_input[i][0] < data_input[j][0]:
+            if data_input[i][1] <= data_input[j][1]:
+                data_input[i][2] += 1
