a137d9e907702f23b12eec9b1bb18245e65edf15
JunOnJuly <wlgnstjd0717@gmail.com>
2022-06-18 18:31:51 +0900

    220618

--- python_code/BaekJoon/7568.py
+++ python_code/BaekJoon/7568.py
@@ -3,7 +3,7 @@ N = int(input())
 data_input = []
 
 for _ in range(N):
-    data_input.append(list(map(int, input())) + [1])
+    data_input.append(list(map(int, input().split())) + [1])
 
 print(data_input)
 
@@ -14,3 +14,5 @@ for i in range(N):
         if data_input[i][0] < data_input[j][0]:
             if data_input[i][1] <= data_input[j][1]:
                 data_input[i][2] += 1
+
+print(data_input)
