d315730b1cf95c1de49a4efc71a4dade568d0503
JunOnJuly <wlgnstjd0717@gmail.com>
2022-06-19 18:40:24 +0900

    220619

--- python_code/BaekJoon/7568.py
+++ python_code/BaekJoon/7568.py
@@ -10,8 +10,10 @@ for i in range(N):
         if i == j:
             continue
         if data_input[i][0] < data_input[j][0]:
-            if data_input[i][1] <= data_input[j][1]:
+            if data_input[i][1] < data_input[j][1]:
                 data_input[i][2] += 1
 
-for i in range(N):
+for i in range(N-1):
     print(data_input[i][2], end=' ')
+
+print(data_input[N-1][2])
