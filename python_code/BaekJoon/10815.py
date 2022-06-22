46bbb9c56b39ea4d5f292b4edde4651119a9ba25
JunOnJuly <wlgnstjd0717@gmail.com>
2022-06-22 14:22:58 +0900

    220621

--- python_code/BaekJoon/10815.py
+++ python_code/BaekJoon/10815.py
@@ -3,4 +3,13 @@ num_input = list(map(int, input().split()))
 M = int(input())
 num_differ = list(map(int, input().split()))
 
+list_bool = []
+
 for i in range(len(num_differ)):
+    for j in range(len(num_input)):
+        if num_differ[i] == num_input[j]:
+            list_bool.append(1)
+        elif i == len(num_differ) - 1:
+            list_bool.append(0)
+
+print(num_differ)
