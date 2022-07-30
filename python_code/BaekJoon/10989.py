1420945c2a63f8404c89ad62beaf490df65c9570
JunOnJuly <wlgnstjd0717@gmail.com>
2022-07-30 22:37:07 +0900

    220730

--- python_code/BaekJoon/10989.py
+++ python_code/BaekJoon/10989.py
@@ -1,6 +1,11 @@
 def sorting():
-    pass
+    return 0
 
 
 N = int(input())
 num_list = [int(input()) for _ in range(N)]
+
+sorted_list = sorting(num_list)
+
+for num in sorted_list:
+    print(num)
