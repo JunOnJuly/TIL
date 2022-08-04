c0c3c7c7eaebaaab45c853e9dba916085e2e591f
JunOnJuly <wlgnstjd0717@gmail.com>
2022-08-04 22:48:43 +0900

    220804

--- python_code/BaekJoon/10989.py
+++ python_code/BaekJoon/10989.py
@@ -5,5 +5,16 @@ num_list = []
 for i in range(N):
     num = int(input())
 
-    for j in range(num_list):
-        if num_list:
+    idx_new = 0
+    if num_list:
+        for j in range(num_list - 1):
+            if num_list[j] < num:
+                idx_new += 1
+            else:
+                num_list.insert(idx_new, num)
+                break
+
+    else:
+        num_list.append(num)
+
+print(num_list)
