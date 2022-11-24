7b7adaf7e276cd29daa6360e41a68897e9bd1178
JunOnJuly <97649331+JunOnJuly@users.noreply.github.com>
2022-11-24 17:53:14 +0900

    update

--- python_code/algorithm/Multi_Variate_Linear_Regression.py
+++ python_code/algorithm/Multi_Variate_Linear_Regression.py
@@ -3,49 +3,93 @@ import matplotlib.pyplot as plt
 import math
 
 
-def make_random_points(num_data, limit_x):
-    degree_trigger = random.randint(0, 2)
-
-    if degree_trigger:
-        degree = random.uniform(3, 5)
-    else:
-        degree = random.uniform(-5, -3)
-    print(degree)
-    point_list_x = []
-    point_list_y = []
+def make_random_points(num_data, limit_x, limit_y):
+    point_list_x = [-5, -3, -1, 1, 3, 5]
+    point_list_y = [random.randint(-limit_y, limit_y + 1) for _ in range(num_data)]
     point_list_xy = []
-
     for i in range(num_data):
-        random_x = random.randint(-limit_x, limit_x+1)
-        random_y = random.gauss(mu=random_x*degree, sigma=1000)
+        point_list_xy.append([point_list_x[i], point_list_y[i]])
 
-        point_list_x.append(random_x)
-        point_list_y.append(random_y)
-        point_list_xy.append([random_x, random_y])
 
     return point_list_x, point_list_y, point_list_xy
 
 
 def calculate_cost(theta_list, random_data_x, random_data_y, num_data):
-    simple_cost_list = []
-    n = len(theta_list)
+    gradient_cost = []
     cost = 0
+    n = len(theta_list)
     for i in range(num_data):
-        y = 0
+        sum_function = 0
         for j in range(n):
-            y += theta_list[j]*(random_data_x[i]**j)
-            simple_cost_list.append(n/num_data)
-        cost = cost + ((y - random_data_y[i])**2)
-    return cost
+            sum_function += theta_list[j] * (random_data_x[i]**j)
+        cost += (sum_function - random_data_y[i]) ** 2
 
-num_data = 10
-limit_x = 1000
-alpha = 0.000005
+        for j in range(n):
+            gradient_cost.append((sum_function - random_data_y[i]) * (random_data_x[i]**j) / num_data)
 
-theta_list = [0, 1, 1, 1]
+    cost = cost / (2*num_data)
 
-random_data_x, random_data_y, random_data_xy = make_random_points(num_data, limit_x)
+    return cost, gradient_cost
+
+
+def calculate_gradient(theta_list, random_data_x, random_data_y, num_data, alpha):
+    min_cost = math.inf
+    min_count = 0
+    theta_list = theta_list
+    n = len(theta_list)
+
+    while True:
+        print(min_count)
+        for i in range(n):
+            if i:
+                print(f' + {theta_list[i]}x^{i}', end='')
+                if i == n-1:
+                    print('')
+            else:
+                print(f'h(x) = {theta_list[i]}', end='')
+
+        cost, theta_list_cal = calculate_cost(theta_list, random_data_x, random_data_y, num_data)
+        print(cost)
+        alpha = 10**-9
+        for i in range(n):
+            theta_list_cal[i] = round(theta_list[i] - (alpha*theta_list_cal[i]), 4)
+
+        if min_cost > cost:
+            min_cost = cost
+            min_count = 0
+            for i in range(n):
+                theta_list[i] = theta_list_cal[i]
+
+        else:
+            if min_count == 10:
+                break
+            min_count += 1
+
+    return theta_list
+
+num_data = 6
+limit_x = 5
+limit_y = 1000
+alpha = 1
+
+theta_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+random_data_x, random_data_y, random_data_xy = make_random_points(num_data, limit_x, limit_y)
+theta_list = calculate_gradient(theta_list, random_data_x, random_data_y, num_data, alpha)
+
+plot_input_x = [i*0.01 for i in range(-limit_x*100, limit_x+1*100)]
+plot_input_y = []
+
+for i in range(len(plot_input_x)):
+    y_cal = 0
+    for j in range(len(theta_list)):
+        y_cal += (plot_input_x[i]**j)*theta_list[j]
+    plot_input_y.append(y_cal)
 
 plt.plot(random_data_x, random_data_y, 'k.')
+plt.plot(plot_input_x, plot_input_y, 'r')
+
+print(plot_input_x, plot_input_y)
+plt.ylim([-limit_y, limit_y])
 
 plt.show()
--- python_code/algorithm/Simple_Linear_Regression.py
+++ python_code/algorithm/Simple_Linear_Regression.py
@@ -45,6 +45,7 @@ def calculate_gradient(theta_0, theta_1, random_data_x, random_data_y, num_data,
     min_theta_1 = 1
 
     while True:
+        print(f'h(x) = {theta_0} + {theta_1}x')
         cost, simple_cost_0, simple_cost_1 = calculate_cost(theta_0, theta_1, random_data_x, random_data_y, num_data)
 
         theta_0 = round(theta_0 - (alpha*(simple_cost_0)), 4)
@@ -72,10 +73,7 @@ theta_0 = 0
 theta_1 = 1
 
 random_data_x, random_data_y, random_data_xy = make_random_points(num_data, limit_x)
-cost = calculate_cost(theta_0, theta_1, random_data_x, random_data_y, num_data)
 theta_0, theta_1 = calculate_gradient(theta_0, theta_1, random_data_x, random_data_y, num_data, alpha)
-
-print(f'h(x) = {theta_0} + {theta_1}x')
 
 plt.plot(random_data_x, random_data_y, 'k.')
 plt.plot([-limit_x, limit_x], [-limit_x*theta_1 + theta_0, limit_x*theta_1 + theta_0], 'r')
